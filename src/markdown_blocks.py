832b40fc6b97d100210090e40ec4060cacdc0362
git-pulver <mpulver3@gmail.com>
2025-02-20 23:20:50 -0500

    mild cheating - Block to HTML

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from textnode import *
+from htmlnode import *
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -16,7 +18,6 @@ def markdown_to_blocks(markdown):
         if s != "":
             new_list.append(s.strip("\n"))
     return new_list
-
 
 def block_to_block_type(block):
     if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
@@ -46,3 +47,91 @@ def block_to_block_type(block):
         return BlockType.PARAGRAPH
     else: 
         return BlockType.PARAGRAPH
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.PARAGRAPH:
+            children.append(paragraph_to_html_node(block))
+        elif block_type == BlockType.CODE:
+            children.append(code_to_html_node(block))
+        elif block_type == BlockType.HEADING:
+            children.append(heading_to_html_node(block))
+        elif block_type == BlockType.OLIST:
+            children.append(olist_to_html_node(block))
+        elif block_type == BlockType.ULIST:
+            children.append(ulist_to_html_node(block))
+        elif block_type == BlockType.QUOTE:
+            children.append(quote_to_html_node(block))
+        else:
+            raise ValueError("invalid block type")
+    return ParentNode("div", children, None)
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+def code_to_html_node(block):
+    if block.startswith("```") and block.endswith("```"):
+        text = block[4:-3]
+        children = text_to_children(text)
+        code = ParentNode("code", children)
+        code_list = [code]
+        return ParentNode("pre", code_list)
+    else:
+        raise ValueError("Invalid formatting - code")
+
+def heading_to_html_node(block):
+    header_size = 1
+    for c in block:
+        if c == "#":
+            header_size += 1
+        else:
+            break
+    if header_size  >= len(block):
+        raise ValueError(f"Invalid formatting - Header")
+    text = block[header_size:]
+    children = text_to_children(text)
+    return ParentNode(f"h{header_size - 1}", children)
+
+def olist_to_html_node(block):
+    items = block.split("\n")
+    item_list = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        item_list.append(ParentNode("li", children))
+    return ParentNode("ol", item_list)
+
+def ulist_to_html_node(block):
+    items = block.split("\n")
+    item_list = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        item_list.append(ParentNode("li", children))
+    return ParentNode("ul", item_list)
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if line.startswith(">") != True:
+            raise ValueError("Invalid formatting - quote")
+        new_lines.append(line.lstrip(">").strip())
+    text = " ".join(new_lines)
+    children = text_to_children(text)
+    return ParentNode("blockquote", children)
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
